ecc41cb178945a4da49f3dbc9af8cdcf1a671935
JersonZambrano <zambranojerson@hotmail.es>
2015-10-12 13:26:00 -0500

    Personaje Con  movimiento
    se establecieron los movimientos del personaje, y la posición en que la
    cama será puesta

---
+++ LeishGame/Assets/Velocidad_movimiento.js
@@ -0,0 +1,11 @@
+﻿var velocidad=5.0;
+
+function Update () {
+var x:Vector3=Input.GetAxis("Horizontal")*transform.right*Time.deltaTime*velocidad;
+var z:Vector3=Input.GetAxis("Vertical")*transform.forward*Time.deltaTime*velocidad;
+
+transform.Traslate(x+z);
+
+//transform.Translate(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
+
+}
---
+++ LeishGame/Assets/movimientoPersonaje.js
@@ -0,0 +1,5 @@
+﻿
+function Update () {
+transform.Translate(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
+
+}
