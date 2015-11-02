703aee60000875525f5b325733580426ebec14dd
Dapu8812 <pigda8812@gmail.com>
2015-11-02 15:27:02 -0500

    Vida del personaje
    se agrega scripts para el funcionamiento de las vidas del personaje con
    (Vida_Corazones.cs y quitar_vida.cs)

---
+++ Assets/Scripts/Quitar_Vida.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quitar_Vida : MonoBehaviour {
+
+
+    // OnTriggerEnter cuando el se entra al objeto cambia el valor de  VIDA_Corazones.vida en -1 simulando la perda de vida
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("enemigo"))
+        {
+            Vida_Corazones.vida = Vida_Corazones.vida - 1;
+            if (Vida_Corazones.vida <= 0)
+            {//si la vida es menor o igual a 0
+             //aca ponen lo que quieran por ejemplo una animacion de morir o nc, un texto que diga estas muerto etc
+            }
+        }
+        if (other.gameObject.CompareTag("moneda"))
+        {
+            Vida_Corazones.vida = Vida_Corazones.vida + 1;
+        }
+
+
+    }
+
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Vida_Corazones.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vida_Corazones : MonoBehaviour
+{
+
+    public GUITexture image;
+    public Texture2D Image_01;
+    public Texture2D Image_02;
+    public Texture2D Image_03;
+    public Texture2D Image_04;
+
+    public static int vida = 3; // variable vida
+
+    void Update()
+    {
+        if (vida == 3)
+        {
+            image.texture = Image_01;
+        }
+        if (vida == 2)
+        {
+            image.texture = Image_02;
+        }
+        if (vida == 1)
+        {
+            image.texture = Image_03;
+        }
+        if (vida == 0)
+        {
+            image.texture = Image_04;
+        }
+
+    }
+}
