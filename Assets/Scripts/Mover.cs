fa01c0b145a9bb8c7765506f4de23f2b0fc2bde7
Mateo Toquica A <mateotoquica@gmail.com>
2015-11-02 20:58:22 -0500

    Test a punto
    Pruebas unitarias a punto de estar. Lo unico que falta es darle detalles
    y ya solo falta subirlo a jenkins

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+	public bool collision=false;
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (new Vector3 (0.2f, 0, 0), Space.Self);
+	}
+
+	void OnCollisionStay(Collision col)
+	{
+		collision = true;
+		this.GetComponent<Rigidbody> ().isKinematic = false;
+	}
+}
