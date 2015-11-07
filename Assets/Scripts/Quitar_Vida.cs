35502d97dca2f7a54c5eafe165557aaa8798a2d0
Mateo Toquica A <mateotoquica@gmail.com>
2015-11-07 13:48:26 -0500

    moneda pausa
    el personaje se pausa al momento de tocar las "monedas"

--- Assets/Scripts/Quitar_Vida.cs
+++ Assets/Scripts/Quitar_Vida.cs
@@ -18,6 +18,8 @@ public class Quitar_Vida : MonoBehaviour {
         if (other.gameObject.CompareTag("moneda"))
         {
             Vida_Corazones.vida = Vida_Corazones.vida + 1;
+			Time.timeScale=0;
+
         }
 
 
