79e9f60218b7d459e7ffdadd0b2b4affbfd3b60d
Mateo Toquica A <mateotoquica@gmail.com>
2015-11-09 11:45:58 -0500

    corrección de errores
    Corrección de errores doble vida y quitar dos vidas

--- Assets/Scripts/Quitar_Vida.cs
+++ Assets/Scripts/Quitar_Vida.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class Quitar_Vida : MonoBehaviour {
 
+	void reset()
+	{
+		Application.LoadLevel("Final");
+	}
+
 
     // OnTriggerEnter cuando el se entra al objeto cambia el valor de  VIDA_Corazones.vida en -1 simulando la perda de vida
     void OnTriggerEnter(Collider other)
@@ -13,8 +18,9 @@ public class Quitar_Vida : MonoBehaviour {
             GameObject.FindGameObjectWithTag("pj").transform.position = new Vector4(-21, 3, 0);
             if (Vida_Corazones.vida <= 0)
             {//si la vida es menor o igual a 0
-				Application.LoadLevel("Final");
 				Vida_Corazones.vida = 3;
+				reset();
+
              //aca ponen lo que quieran por ejemplo una animacion de morir o nc, un texto que diga estas muerto etc
             }
         }
--- Assets/Scripts/Quitar_Vida2.cs
+++ Assets/Scripts/Quitar_Vida2.cs
@@ -3,7 +3,10 @@ using System.Collections;
 
 public class Quitar_Vida2 : MonoBehaviour {
 
-
+	void reset()
+	{
+		Application.LoadLevel("Final");
+	}
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("enemigo"))
@@ -12,8 +15,9 @@ public class Quitar_Vida2 : MonoBehaviour {
             GameObject.FindGameObjectWithTag("pj").transform.position = new Vector4(13, 3, -15);
             if (Vida_Corazones.vida <= 0)
             {//si la vida es menor o igual a 0
-				Application.LoadLevel("Final");
 				Vida_Corazones.vida = 3;
+				reset ();
+	
              //aca ponen lo que quieran por ejemplo una animacion de morir o nc, un texto que diga estas muerto etc
             }
         }
