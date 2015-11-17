1699ad46ac48baf07664a99d2c52ef85db189126
JersonZambrano <zambranojerson@hotmail.es>
2015-11-17 04:06:37 -0500

    Tutorial
    Se agrego una nueva vista de tutorial al juego

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tutorial : MonoBehaviour {
+	private int clicks;
+
+	// Use this for initialization
+	void Start () {
+	clicks = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	if (clicks > 1)
+		{
+			OnMouseUp();   
+		}
+	}
+	void OnMouseUp ()
+	{
+		Application.LoadLevel ("Tutorial");
+	}
+}
