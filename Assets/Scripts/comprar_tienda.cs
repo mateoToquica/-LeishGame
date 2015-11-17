5ebabf7260f563d59f2cc3b33cf64fa01df32277
danielAlvarado <pigda8812@gmail.com>
2015-11-17 05:18:43 -0500

    escena tienda funcionando
    se agrega la nueva escena y los script para su funcionamiento

---
+++ Assets/Scripts/comprar_tienda.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class comprar_tienda : MonoBehaviour {
+
+
+    private int clicks;
+
+    // Use this for initialization
+    void Start()
+    {
+        clicks = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (clicks > 1)
+        {
+            OnMouseUp();
+        }
+    }
+
+    void OnMouseUp()
+    {
+        sumar_Moneda.moneda= sumar_Moneda.moneda - 1;
+    }
+}
